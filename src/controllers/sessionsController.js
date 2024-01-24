66b964c5fe64f7756a04be77e0bfa994184d2e2d
AzulSchygiel <132393983+AzulSchygiel@users.noreply.github.com>
2024-01-24 12:51:03 -0300

    Update sessionsController.js

--- src/controllers/sessionsController.js
+++ src/controllers/sessionsController.js
@@ -1,19 +1,70 @@
-import Usuario from "../dao/UsuariosDao.js";
-import { usuariosService } from "../services";
+port {generarEmailToken, sendChangeContraseñadeEmail, verifyEmailToken} from "../helpers/email.js";
+import { UsuariosService } from "../services/usuariosService.js";
+import { crearHash, invalidContraseña } from "../utils.js";
 
-const iniciarsesion = async(req,res) => {
-    const {email, password} = req.body;
-    if(!email || !password) return res.status(400).send({status:"error", error:"Valores incompletos"});
-    const usuario = await usuariosService.getUsuarioByEmail(email);
-    if(!usuario) return res.status(404).send({status:"error", error:"El usuario no existe"});
-    const isValidPassword = await passwordValidation(usuario, password);
-    if(!isValidPassword) return res.status(400).send({status:"error", error:"Contraseña incorrecta"});
-    const usuarioDto = UsuarioDTO.getUsuarioTokenFrom(usuario);
-    const token= jwt.sign(usuarioDto, 'tokenSecretJWT', {expiresIn:"1h"});
-    res.cookie('coderCookie',token,{maxAge:3000000}).send({status:"success", message:"Se inició sesión"})
-}
+export class SessionesController{
+    static redirectIniciarsesion = (req,res)=>{
+        res.redirect("/iniciarsesion");
+    };
 
-const current = async(req,res) => {
-    const cookie = req.cookies['coderCookie']
-    const usuario = jwt.verify(cookie, 'tokenSecretJWT');
-}
+    static failRegistro = (req,res)=>{
+        res.render("registrarse", {error:"ERROR (no se pudo registrar al usuario, inténtelo más tarde...)"});
+    };
+
+    static redirectPerfil = (req,res)=>{
+        res.render("perfilView", {usuario:req.usuario});
+    };
+
+    static failIniciodesesion = (req,res)=>{
+        res.render("iniciarsesion", {error:"ERROR (no se logró iniciar sesion al usuario, inténtelo más tarde..."});
+    };
+
+    static forgotContraseña = async(req,res)=>{
+        const {email} = req.body;
+        console.log(email);
+        try {
+            const usuario =await UsuariosService.getUsuarioByEmail(email);
+            const emailToken = generarEmailToken(email, 10 * 30)
+            await sendChangeContraseñadeEmail(req,email,emailToken);
+            res.send(`Se envió un enlace a su correo <a href="/"> Volver a la página de inicio de sesión </a>`);
+        } catch (error) {
+            res.json({status:"error", message:error.message});
+        }
+    };
+
+    static resetContraseña = async(req,res)=>{
+        try {
+            const token = req.query.token;
+            const {newContraseña} = req.body;
+            const validEmail = verifyEmailToken(token);
+            if (!validEmail) {
+                return res.send(`El enlace ya no es válido, genera uno nuevo haciendo click aqui <a href="/forgot-contraseña">enlace</a>`);
+            }
+            const usuario = await UsuariosService.getUsuarioByEmail(validEmail);
+            console.log("usuario", usuario);
+            if(!usuario){
+                return res.send(`ERROR (no es valido)`);
+            }
+            if(invalidContraseña(newContraseña,usuario)){
+                return res.render("resetPassView", {error:"ERROR (contraseña incorrecta)",token});
+            }
+            const usuarioData = {
+                ...usuario,
+                contraseña:crearHash(newContraseña)
+            };
+            await UsuariosService.updateUsuario(usuario._id, usuarioData);
+            res.render("iniciarsesion", {message:"Contraseña actualizada correctamente"});
+        } catch (error) {
+            res.json({status:"error", message:error.message});
+        }
+    };
+
+    static cerrarsesion = async(req,res)=>{
+        console.log(req.usuario);
+        const usuario = {...req.usuario};
+        usuario.last_connection = new Date();
+        await UsuariosService.updateUsuario(usuario._id, usuario);
+        req.session.destroy((error)=>{
+            res.send("Fin de la sesión");
+        });
+    };
